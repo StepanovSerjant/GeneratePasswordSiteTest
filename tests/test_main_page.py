7f7ca4ff81cfd14dce881643206c72c1e95febdc
Aleksey Stepanov <stpnvlks@gmail.com>
2020-11-20 21:58:13 +0300

    Implemented some main page tests.

---
+++ tests/test_main_page.py
@@ -0,0 +1,38 @@
+import pytest
+
+from pages.main_page import MainPage
+from tests.test_base import BaseTest
+
+
+class TestMain(BaseTest):
+    
+    def test_numbers_checkbox(self):
+        self.main_page = MainPage(self.driver)
+        self.main_page.set_checkbox(self.main_page.NUMBERS_CHECKBOX, True)
+        new_password = self.main_page.get_new_password()
+        assert list(filter(lambda x: x in new_password, list("0123456789")))
+
+    def test_uppercase_checkbox(self):
+        self.main_page = MainPage(self.driver)
+        self.main_page.set_checkbox(self.main_page.UPPERCASE_CHECKBOX, True)
+        new_password = self.main_page.get_new_password()
+        assert list(filter(lambda x: x in new_password, list(new_password.upper())))
+
+    def test_lowercase_checkbox(self):
+        self.main_page = MainPage(self.driver)
+        self.main_page.set_checkbox(self.main_page.LOWERCASE_CHECKBOX, True)
+        new_password = self.main_page.get_new_password()
+        assert list(filter(lambda x: x in new_password, list(new_password.lower())))
+
+    def test_autoselect_checkbox(self):
+        self.main_page = MainPage(self.driver)
+        self.main_page.set_checkbox(self.main_page.AUTOSELECT_CHECKBOX, True)
+        new_password = self.main_page.get_new_password()
+        assert new_password == self.main_page.get_selected_text()
+
+    def test_selecting_length(self):
+        testing_length = 16
+        self.main_page = MainPage(self.driver)
+        self.main_page.set_length(testing_length)
+        new_password = self.main_page.get_new_password()
+        assert len(new_password) == testing_length
